08694930354d3719b6b5bea82d82b545c79b1436
pedrorijo91 <pedrorijo91@gmail.com>
2015-05-19 22:51:51 +0100

    Add update script

---
+++ update.sh
@@ -0,0 +1,13 @@
+echo "wget canteen information file from endpoint";
+wget -O tmp0 https://fenix.tecnico.ulisboa.pt/api/fenix/v1/canteen;
+echo "Applying double quote escaping";
+sed "s/\"/\\\\\"/g" tmp0 > tmp1;
+echo "Adding surrounding double quotes";
+echo "\"" | cat - tmp1 > tmp2
+echo "\"" >> tmp2
+echo "Removing newline ocurrences";
+awk 1 ORS=' ' tmp2 > tmp3;
+echo "Moving file to right directory";
+cp tmp3 assets/resources/data.json;
+echo "Removing tmp files";
+rm tmp*;
